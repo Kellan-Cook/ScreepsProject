0bd18997542560311ea0f09b81357d6fd342fc29
gnomagin Dallocort <email@email.com>
2024-02-28 02:20:45 -0500

    auto formated all code with prettier

--- role.rangedefender.js
+++ role.rangedefender.js
@@ -1,20 +1,13 @@
 var rolerangedefender = {
+  /** @param {Creep} creep **/
+  run: function (creep) {
+    var targets = creep.room.find(FIND_HOSTILE_CREEPS);
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
-
-        var targets = creep.room.find(FIND_HOSTILE_CREEPS)
-
-        if(targets.length > 0){
-
-            if(creep.rangedAttack(targets[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
-
-        }
-
-
+    if (targets.length > 0) {
+      if (creep.rangedAttack(targets[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(targets[0], { visualizePathStyle: { stroke: "#ffaa00" } });
+      }
     }
-
-}
-module.exports = rolerangedefender
+  },
+};
+module.exports = rolerangedefender;
