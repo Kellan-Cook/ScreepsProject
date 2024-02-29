b46da402114693265f297db10e4761a0f22b4d90
gnomagin Dallocort <email@email.com>
2024-02-29 15:10:41 -0500

    fixed acidental code replace in role repair

--- role.repair.js
+++ role.repair.js
@@ -1,60 +1,78 @@
-var rolestoragemanager = {
-  /** @param {Creep} creep **/
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('role.repair');
+ * mod.thing == 'a thing'; // true
+ */
 
-  //starts the function
+var rolerepair = {
   run: function (creep) {
-    //sets the task memory state based on used capacity and if changes to withdraw sets the target storage id
-    if (creep.store.getUsedCapacity() == creep.store.getCapacity) {
-      creep.memory.task = "storing";
+    //if memory is set to true and carrying 0 energy change to harvesting mode
+    if (creep.memory.repairing && creep.store[RESOURCE_ENERGY] == 0) {
+      creep.memory.repairing = false;
+      creep.say("🔄 harvest");
+    }
+    // if memory is set to false and free energy capacity is equal to 0 set memory to repairing
+    if (!creep.memory.repairing && creep.store.getFreeCapacity() == 0) {
+      creep.memory.repairing = true;
+      creep.say("🛠️ repairing");
     }
 
-    if (creep.store.getUsedCapacity() == 0) {
-      creep.memory.task = "withdrawing";
-
-      var closest = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+    //if not repairing find energy to replenish on
+    if (!creep.memory.repairing) {
+      var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
         filter: (structure) => {
           return (
             (structure.structureType == STRUCTURE_CONTAINER ||
               structure.structureType == STRUCTURE_STORAGE) &&
-            structure.store.getUsedCapacity(RESOURCE_ENERGY) != 0
+            structure.store.getUsedCapacity(RESOURCE_ENERGY) > 1
           );
         },
       });
 
-      creep.memory.currentmove = closest.id;
-    }
-
-    //runs the task of moving to and collecting from storage structures
-    if (creep.memory.task == "withdrawing") {
-      if (creep.memory.currentmove != null) {
-        var targets = Game.getObjectById(creep.memory.currentmove);
-
-        if (creep.withdraw(targets, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-          creep.moveTo(targets);
+      if (target != null) {
+        if (creep.withdraw(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(target);
         }
       }
     }
 
-    //runs the task of moving to epty extensions and filling
-    if ((creep.memory.task = "storing" && creep.memory.task != "withdrawing")) {
-      var targetstorage = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+    //if memory is repairing find closest struture to repair
+    if ((creep.memory.repairing = true)) {
+      var target = creep.room.find(FIND_STRUCTURES, {
         filter: (structure) => {
           return (
-            (structure.structureType == STRUCTURE_SPAWN ||
-              structure.structureType == STRUCTURE_EXTENSION) &&
-            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+            structure.hits < structure.hitsMax &&
+            structure.hits < 2000000 &&
+            structure.structureType != STRUCTURE_WALL
           );
         },
       });
+      target.sort((a, b) => a.hits - b.hits);
+    }
 
-      if (targetstorage != null) {
-        if (
-          creep.transfer(targetstorage, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE
-        ) {
-          creep.moveTo(targetstorage);
-        }
+    if (target[0] != null)
+      if (creep.repair(target[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(target[0]);
+      } else {
+        var target = creep.room.find(FIND_STRUCTURES, {
+          filter: (structure) => {
+            return (
+              structure.hits < structure.hitsMax && structure.hits < 2000000
+            );
+          },
+        });
+        target.sort((a, b) => a.hits - b.hits);
+      }
+
+    if (target[0] != null) {
+      if (creep.repair(target[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(target[0]);
       }
     }
   },
 };
-module.exports = rolestoragemanager;
+
+module.exports = rolerepair;
